4f85daef45d6e78d38df49e3378f178d5242d9ec
beerent <brentryczak@gmail.com>
2020-03-04 17:51:17 -0600

    added product table to creation script

--- database/creation_script.sql
+++ database/creation_script.sql
@@ -7,3 +7,13 @@ CREATE TABLE `shopify`.`users` (
 
 ALTER TABLE `shopify`.`users` 
 ADD COLUMN `added` DATETIME NULL AFTER `email`;
+
+CREATE TABLE `shopify`.`products` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(100) NOT NULL,
+  `description` VARCHAR(300) NULL,
+  PRIMARY KEY (`id`));
+
+ALTER TABLE `shopify`.`products` 
+ADD COLUMN `added` DATETIME NULL AFTER `description`;
+
