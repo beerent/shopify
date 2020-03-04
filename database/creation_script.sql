ed9d4469d3a8d4925481eb5d8777618070bebd31
beerent <brentryczak@gmail.com>
2020-03-04 17:43:53 -0600

    added user table to creation script

---
+++ database/creation_script.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `shopify`.`users` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `first_name` VARCHAR(45) NULL,
+  `last_name` VARCHAR(45) NULL,
+  `email` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id`));
