96953f8ed612d2df3f7e49ecedbd7cb9b59c44a4
beerent <brentryczak@gmail.com>
2020-03-04 17:51:34 -0600

    added orders table to creation script

--- database/creation_script.sql
+++ database/creation_script.sql
@@ -17,3 +17,13 @@ CREATE TABLE `shopify`.`products` (
 ALTER TABLE `shopify`.`products` 
 ADD COLUMN `added` DATETIME NULL AFTER `description`;
 
+CREATE TABLE `shopify`.`orders` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `user_id` VARCHAR(45) NOT NULL,
+  `ordered` DATETIME NOT NULL,
+  PRIMARY KEY (`id`),
+  CONSTRAINT `order_user_id_fk`
+    FOREIGN KEY (`id`)
+    REFERENCES `shopify`.`users` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);
