4e851f09ae23e7253d1f4ace76558389fdc76a38
beerent <brentryczak@gmail.com>
2020-03-04 22:05:53 -0600

    adding endpoint skeleton

---
+++ backend/src/main/java/com/example/demo/DeleteOrdersEndpoint.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class DeleteOrdersEndpoint {
+
+    @GetMapping("/delete")
+    @ResponseBody
+    public String HelloWorld() {
+        return "deleting orders!";
+    }
+
+}
