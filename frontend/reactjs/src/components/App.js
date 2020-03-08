2f4c66909caf3da7176f0f5cf302f13ad6afdff3
beerent <brentryczak@gmail.com>
2020-03-08 07:52:57 -0500

    react front end successfully hitting cors-friendly spring backend

--- frontend/reactjs/src/components/App.js
+++ frontend/reactjs/src/components/App.js
@@ -1,17 +1,26 @@
-    import React, { Component } from 'react';
+import React, { Component } from 'react';
+import Orders from "./Orders"
 
-    class App extends Component {
-      render() {
-        return (
-          <div className="card">
-            <div className="card-body">
-              <h5 className="card-title">Steve Jobs</h5>
-              <h6 className="card-subtitle mb-2 text-muted">steve@apple.com</h6>
-              <p className="card-text">Stay Hungry, Stay Foolish</p>
-            </div>
-          </div>
-        );
-      }
-    }
 
-    export default App;
+class App extends Component {
+  state = {
+    orders : {}
+  };
+
+  componentDidMount() {
+    fetch('http://localhost:8080/get')
+    .then(res => res.json())
+    .then((data) => {
+      this.setState({ contacts: data })
+    })
+    .catch(console.log)
+  }
+
+  render() {
+    return (
+      <Orders />
+    );
+  }
+}
+
+export default App;
