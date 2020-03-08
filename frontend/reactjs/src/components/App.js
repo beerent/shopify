52e32253e65683b9e89b85f11aae9caf5433b8d0
beerent <brentryczak@gmail.com>
2020-03-08 13:03:40 -0500

    disabled buttons while data is loading

--- frontend/reactjs/src/components/App.js
+++ frontend/reactjs/src/components/App.js
@@ -3,50 +3,9 @@ import Orders from "./Orders"
 
 
 class App extends Component {
-  state = {
-    orders : []
-  };
-
-  GetOrders = () => {
-    fetch('http://localhost:8080/get')
-    .then(res => res.json())
-    .then((data) => {
-      this.setState({ orders : data.orders })
-    })
-    .catch(console.log)
-  }
-
-  FetchOrders = () => {
-    fetch('http://localhost:8080/fetch', {
-      method: 'POST',
-      headers: {
-        'Accept': '*/*',
-      },
-      body: JSON.stringify({})
-    }).then(() => {
-      this.GetOrders();
-    });
-  }
-
-  DeleteOrders = () => {
-    fetch('http://localhost:8080/delete', {
-      method: 'DELETE',
-      headers: {
-        'Accept': '*/*',
-      },
-      body: JSON.stringify({})
-    }).then(() => {
-      this.GetOrders();
-    });
-  }
-
-  componentDidMount() {
-    this.GetOrders()
-  }
-
   render() {
     return (
-      <Orders orders={this.state.orders} FetchOrders={this.FetchOrders} DeleteOrders={this.DeleteOrders} />
+      <Orders />
     );
   }
 }
--- frontend/reactjs/src/components/Orders.js
+++ frontend/reactjs/src/components/Orders.js
@@ -2,20 +2,90 @@ import React, { Component } from 'react';
 import Order from "./Order"
 
 class Orders extends Component {
+  isComponentMounted = false;
+
+  state = {
+    orders : []
+  };
+
+  GetOrders = () => {
+    this.DisableButtons();
+
+    fetch('http://localhost:8080/get')
+    .then(res => res.json())
+    .then((data) => {
+      if (this.isComponentMounted) {
+        this.setState({ orders : data.orders })
+      }
+      this.EnableButtons();
+    })
+    .catch(console.log)
+
+  }
+
+  FetchOrders = () => {
+    this.DisableButtons();
+
+    fetch('http://localhost:8080/fetch', {
+      method: 'POST',
+      headers: {
+        'Accept': '*/*',
+      },
+      body: JSON.stringify({})
+    }).then(() => {
+      this.GetOrders();
+    });
+  }
+
+  DeleteOrders = () => {
+    this.DisableButtons();
+
+    fetch('http://localhost:8080/delete', {
+      method: 'DELETE',
+      headers: {
+        'Accept': '*/*',
+      },
+      body: JSON.stringify({})
+    }).then(() => {
+      this.GetOrders();
+    });
+  }
+
+  componentDidMount() {
+    this.isComponentMounted = true;
+    this.GetOrders()
+  }
+
+  componentWillUnmound() {
+    this.isComponentMounted = false;
+  }
+
+
+  EnableButtons() {
+    document.getElementById("fetch_button").disabled = false;
+    document.getElementById("delete_button").disabled = false;
+  }
+
+  DisableButtons() {
+    document.getElementById("fetch_button").disabled = true;
+    document.getElementById("delete_button").disabled = true;
+  }
 
   render() {
     const orderElements = []
 
-    this.props.orders.forEach(function(order, index){
-      orderElements.push(<Order order={order} key={index} />)
-    })
+    if (this.state.orders) {
+      this.state.orders.forEach(function(order, index){
+        orderElements.push(<Order order={order} key={index} />)
+      });
+  }
 
     return (
       <div>
         <div className="col text-center">
           <div className="btn-group">
-            <button className="btn btn-default" className="btn btn-success" onClick={this.props.FetchOrders}>Fetch</button>
-            <button className="btn btn-default" className="btn btn-danger" onClick={this.props.DeleteOrders}>Delete All</button>
+            <button id="fetch_button" className="btn btn-default" className="btn btn-success" onClick={this.FetchOrders}>Fetch</button>
+            <button id="delete_button" className="btn btn-default" className="btn btn-danger" onClick={this.DeleteOrders}>Delete All</button>
           </div> 
         </div>
 
