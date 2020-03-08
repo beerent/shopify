b290291b7dcebcb0490b4938551de2705420f746
beerent <brentryczak@gmail.com>
2020-03-08 15:05:44 -0500

    added loading bar

--- frontend/reactjs/src/components/Order.js
+++ frontend/reactjs/src/components/Order.js
@@ -5,7 +5,7 @@ class Order extends Component {
     return (
       <div className="card mb-3">
       <div className="card-header">Order #{this.props.order.id}</div>
-        <div className="card-body">
+        <div className="card-body" data-toggle="collapse">
           <h5 className="card-title">{this.props.order.user.first_name + " " + this.props.order.user.last_name}</h5>
           <h6 className="card-subtitle mb-2 text-muted">{this.props.order.user.email}</h6>
         </div>
--- frontend/reactjs/src/components/Orders.js
+++ frontend/reactjs/src/components/Orders.js
@@ -12,7 +12,7 @@ class Orders extends Component {
   GetOrders = () => {
     var self = this;
 
-    self.DisableButtons();
+    self.FetchStarted();
 
     fetch('http://localhost:8080/get')
     .then(res => res.json())
@@ -20,10 +20,10 @@ class Orders extends Component {
       if (self.isComponentMounted) {
         self.setState({ orders : data.orders })
       }
-      self.EnableButtons();
+      self.FetchFinished();
     })
     .catch(function(){
-      self.EnableButtons();
+      self.FetchFinished();
     })
 
   }
@@ -31,7 +31,7 @@ class Orders extends Component {
   FetchOrders = () => {
     var self = this;
 
-    self.DisableButtons();
+    self.FetchStarted();
 
     fetch('http://localhost:8080/fetch', {
       method: 'POST',
@@ -43,14 +43,14 @@ class Orders extends Component {
       self.GetOrders();
     })
     .catch(function(){
-      self.EnableButtons();
+      self.FetchFinished();
     })
   }
 
   DeleteOrders = () => {
     var self = this;
 
-    self.DisableButtons();
+    self.FetchStarted();
 
     fetch('http://localhost:8080/delete', {
       method: 'DELETE',
@@ -72,6 +72,16 @@ class Orders extends Component {
     this.isComponentMounted = false;
   }
 
+  FetchFinished() {
+    this.EnableButtons();
+    this.HideLoadingSymbol();
+  }
+
+  FetchStarted() {
+    this.DisableButtons();
+    this.ShowLoadingSymbol();
+  }
+
   EnableButtons() {
     document.getElementById("fetch_button").disabled = false;
     document.getElementById("delete_button").disabled = false;
@@ -82,23 +92,38 @@ class Orders extends Component {
     document.getElementById("delete_button").disabled = true;
   }
 
+  ShowLoadingSymbol() {
+    document.getElementById("loading_symbol").style.display="block";
+  }
+
+  HideLoadingSymbol() {
+    document.getElementById("loading_symbol").style.display="none";
+  }
+
   render() {
     const orderElements = []
 
-    if (this.state.orders) {
       this.state.orders.forEach(function(order, index){
         orderElements.push(<Order order={order} key={index} />)
       });
-  }
     return (
       <div>
         <div className="jumbotron text-center mb-0">
           <h1>E-Commerce Dashboard</h1>
         </div>
-          <div className="text-center mt-4 mb-4">
-            <button id="fetch_button" className="btn" className="btn btn-success button-width mr-4" onClick={this.FetchOrders}>Fetch</button>
-            <button id="delete_button" className="btn" className="btn btn-danger button-width ml-4" onClick={this.DeleteOrders}>Delete All</button>
-          </div> 
+        
+        <div className="text-center mt-4 mb-4">
+          <button id="fetch_button" className="btn" className="btn btn-success button-width mr-4" onClick={this.FetchOrders}>Fetch</button>
+          <button id="delete_button" className="btn" className="btn btn-danger button-width ml-4" onClick={this.DeleteOrders}>Delete All</button>
+        </div> 
+
+        <div id="loading_symbol">
+        <div className="d-flex justify-content-center">
+          <div className="spinner-grow text-success" style={{width: '3rem', height: '3rem'}} role="status">
+            <span className="sr-only">Loading...</span>
+          </div>
+        </div>
+        </div>
 
         <div>{orderElements}</div>
         </div>
