56ca372f24c38445dac2bda148924be13727e87f
beerent <brentryczak@gmail.com>
2020-03-08 07:20:50 -0500

    hello world react with bootstrap working

---
+++ frontend/reactjs/src/components/App.js
@@ -0,0 +1,17 @@
+    import React, { Component } from 'react';
+
+    class App extends Component {
+      render() {
+        return (
+          <div className="card">
+            <div className="card-body">
+              <h5 className="card-title">Steve Jobs</h5>
+              <h6 className="card-subtitle mb-2 text-muted">steve@apple.com</h6>
+              <p className="card-text">Stay Hungry, Stay Foolish</p>
+            </div>
+          </div>
+        );
+      }
+    }
+
+    export default App;
